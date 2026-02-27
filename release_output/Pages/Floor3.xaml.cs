ac141712d9a986f28bfce4d2f931f678991d638f
RENDER <185948264+MihailKashintsev@users.noreply.github.com>
2026-02-27 23:31:43 +0300

    Release v0.0.1

---
+++ release_output/Pages/Floor3.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Kiosk.Pages
+{
+    public partial class Floor3 : Page
+    {
+        public Floor3()
+        {
+            InitializeComponent();
+        }
+
+        private void Room_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is FrameworkElement element && element.Tag is string roomNumber)
+            {
+                var floorPlanWindow = Window.GetWindow(this) as FloorPlanWindow;
+                floorPlanWindow?.ShowRoomInfo(roomNumber);
+                e.Handled = true;
+            }
+        }
+    }
+}
